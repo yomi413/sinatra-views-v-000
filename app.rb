429cea59ed7b8dd9a338c218f4486a909e7e9acd
Yomaira Escano <yjescano@gmail.com>
2018-01-13 04:03:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,9 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
-		"<h1>Hello World</h1>"
-	end
+	# get '/' do
+	# 	"<h1>Hello World</h1>"
+	# end
 
 	get '/' do
 		erb :index
